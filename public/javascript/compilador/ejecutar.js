23a6c682b8f1e55c8aee1a679e3b7062fa7b1958
Manuel <manuel13580@gmail.com>
2019-03-21 13:08:03 -0600

    Done- Funcionalidad Agregar Pesta y obtener el valor de cada una de las pesta

---
+++ public/javascript/compilador/ejecutar.js
@@ -0,0 +1,8 @@
+function btn_compilar(){
+    //vamos a traer al activo del contenedor
+    var elementoDiv=document.querySelector("#contenedor_pesta > div.active");
+    //vamos a traer el area de texto para poder enviarle la informacion al parser
+    var area_texto=document.getElementById(elementoDiv.id+"_input");
+    alert(area_texto.value);
+
+}
---
+++ public/javascript/controladorui/principal.js
@@ -0,0 +1,21 @@
+//tener cuidado con el nombre de las variales
+function crearPesta(){
+    var d=new Date();
+    var n=d.getTime();
+    var content=document.getElementById("contenedor_pesta");
+    var nav=document.getElementById("nav_pesta");
+    var elemento=document.querySelector('#nav_pesta > li.active');
+    elemento.className="";
+    
+    var elementoDiv=document.querySelector("#contenedor_pesta > div.active");
+    elementoDiv.className="tab-pane fade";
+
+    var compo_li="<li class=\"active\"><a data-toggle=\"pill\" href=\"#pesta_"+(content.childNodes.length+1)+"\">Archivo "+n+"</a></li>";
+    nav.insertAdjacentHTML('beforeend',compo_li);
+    var text_area="<div id=\"pesta_"+(content.childNodes.length+1)+"\" class=\"tab-pane fade in active\">"+
+                    "<textarea class=\"form-control rounded-0\" id=\"pesta_text_"+(content.childNodes.length+1)+"_input\" rows=\"10\"></textarea>"+
+                  "</div>";
+    content.insertAdjacentHTML('beforeend',text_area);
+
+
+}
