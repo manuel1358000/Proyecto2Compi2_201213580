a803c755311541567a62163dc15f56c77696a684
Manuel <manuel13580@gmail.com>
2019-04-16 18:16:33 -0600

    Se agregaron los archivos y estructura para For y while

---
+++ public/javascript/AST/Instrucciones/Ciclos/Mientras.js
@@ -0,0 +1,7 @@
+class Mientras{
+    constructor(condicion,nodos){
+        this.condicion=condicion;
+        this.nodos=nodos;
+    }
+
+}
---
+++ public/javascript/AST/Instrucciones/Ciclos/Para.js
@@ -0,0 +1,8 @@
+class Para{
+    constructor(inicializado,condicion,aumento,nodos){
+        this.inicializado=inicializado;
+        this.condicion=condicion;
+        this.aumento=aumento;
+        this.nodos=nodos;
+    }
+}
