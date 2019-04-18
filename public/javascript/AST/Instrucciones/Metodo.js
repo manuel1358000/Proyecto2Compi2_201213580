890736aacf8e20ed2a672a1fe588d27157968981
Manuel <manuel13580@gmail.com>
2019-04-17 22:29:14 -0600

    Se va a agregar la estructura para el if y switch

--- public/javascript/AST/Instrucciones/Metodo.js
+++ public/javascript/AST/Instrucciones/Metodo.js
@@ -69,7 +69,15 @@ class Metodo{
                 }
                 result.cadena+=temp;
             }else if(this.nodos[i] instanceof Si){
-                console.log("Es un if");
+                var ambi=this.ambitos+"/"+this.id;
+                this.nodos[i].ambito=ambi;
+                var result_temp=this.nodos[i].execute(entorno);
+                //aca no vamos a recibir ninguna etiqueta ya que solo se ejecuta el if
+                if(result_temp!=null){
+                    result.cadena+="//EMPIEZA LA EJECUCION DE LA SENTENCIA IF";    
+                    result.cadena+=result_temp.cadena;
+                    result.cadena+="//TERMINA LA EJECUCION DE LA SENTENCIA IF";
+                }
             }else if(this.nodos[i] instanceof Selecciona){
                 console.log("Es un switch");
             }else{
--- public/javascript/AST/Instrucciones/Seleccion/Si.js
+++ public/javascript/AST/Instrucciones/Seleccion/Si.js
@@ -4,8 +4,28 @@ class Si{
         this.nodos=nodos;
         this.subifs=subifs;
         this.defecto=defecto;
+        this.ambito="";
     }
     execute(entorno){
-
+        var local=new Entorno(entorno);
+        //vamos a evaluar la condicion del if principal
+        var result_condi=this.condicion.getValue(entorno);
+        var tipo_condi=this.condicion.getTipe(entorno);
+        if(result_condi!=null){
+            if(tipo_condi=="BOOLEAN"){
+                //aqui tengo que evaluar el camino por el cual se va a tomar la decision
+                
+            }else{
+                alert("Error Semantico, la condicion del if debe de ser booleana");
+            }
+        }
+        if(this.subifs.length>0){
+            //aqui vamos a verificar los nodos que tiene los else if
+            console.log("Evaluando else if");
+        }
+        if(this.defecto!=null){
+            //aqui vamos a verificar el nodo del else
+            console.log("Evaluando else");    
+        }
     }
 }
