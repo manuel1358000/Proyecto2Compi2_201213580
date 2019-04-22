177e2169a55217136731ab328f3eed842df03d77
Manuel <manuel13580@gmail.com>
2019-04-22 11:33:04 -0600

    DO WHILE-DONE

--- public/javascript/AST/Instrucciones/Ciclos/Mientras.js
+++ public/javascript/AST/Instrucciones/Ciclos/Mientras.js
@@ -133,8 +133,116 @@ class Mientras{
                 alert("Error Semantico, condicion del while no valida");
             }
 
-        }else{  
-            alert("Es un do while");
+        }else{
+            pool_salida.push(etisalida);
+            temp+=etiinicio+":\n";
+            for(var i=0;i<this.nodos.length;i++){
+                if(this.nodos[i] instanceof Declaracion){
+                    var ambi=this.ambitos;
+                    this.nodos[i].ambitos=ambi;
+                    var result_temp=this.nodos[i].execute(local);
+                    if(result_temp!=null){
+                        temp+=result_temp.cadena;
+                        temp+="//declaracion variable local\n";
+                        var temph=generarEtiqueta();
+                        temp+=temph+"=h;\n";
+                        temp+="heap[h]="+result_temp.u_etiqueta+";\n";
+                        temp+="h=h+1;\n";
+                        var simulado=generarEtiqueta();
+                        var sim=local.obtener(this.nodos[i].id+"_"+ambi);
+                        if(sim!=null){
+                            temp+=simulado+"=p+"+sim.posRel+";\n";
+                            temp+="stack["+simulado+"]="+temph+";\n";
+                        }else{
+                            alert("Error semantico, no se cuentra el simbolo en la tabla de simbolos");
+                        }
+                        temp+="//fin declaracion variable local\n";
+                    }else{
+                        temp+="//declaracion variable local\n";
+                        var temph=generarEtiqueta();
+                        temp+=temph+"=h;\n";
+                        temp+="heap[h]=0;\n";
+                        temp+="h=h+1;\n";
+                        var simulado=generarEtiqueta();
+                        var sim=local.obtener(this.nodos[i].id+"_"+ambi);
+                        temp+=simulado+"=p+"+sim.posRel+";\n";
+                        temp+="stack["+simulado+"]="+temph+";\n";
+                        temp+="//fin declaracion variable local\n";
+                    }
+                }else if(this.nodos[i] instanceof Imprimir){
+                    this.nodos[i].ambitos=this.ambitos;
+                    var result_temp=this.nodos[i].execute(local);
+                    temp+=result_temp.cadena;
+                }else if(this.nodos[i] instanceof Asignacion){
+                    var ambi=this.ambitos;
+                    this.nodos[i].ambitos=ambi;
+                    var result_temp=this.nodos[i].execute(local);
+                    if(result_temp!=null){
+                        temp+=result_temp.cadena;
+                        temp+="//empieza la asignacion variable local\n";
+                        var temph=generarEtiqueta();
+                        temp+=temph+"=h;\n";
+                        temp+="heap[h]="+result_temp.u_etiqueta+";\n";
+                        temp+="h=h+1;\n";
+                        var simulado=generarEtiqueta();
+                        alert(this.nodos[i].id+"_"+ambi);
+                        var sim=local.obtener(this.nodos[i].id+"_"+ambi);
+                        temp+=simulado+"=p+"+sim.posRel+";\n";
+                        temp+="stack["+simulado+"]="+temph+";\n";
+                        temp+="//fin asignacion variable local\n";
+                    }else{
+                        alert("es nulo");
+                    }
+                }else if(this.nodos[i] instanceof Si){
+                    var ambi=this.ambitos;
+                    this.nodos[i].ambitos=ambi;
+                    var result_temp=this.nodos[i].execute(local);
+                    //aca no vamos a recibir ninguna etiqueta ya que solo se ejecuta el if
+                    if(result_temp!=null){  
+                        temp+=result_temp.cadena;
+                    }
+                }else if(this.nodos[i] instanceof Selecciona){
+                    var ambi=this.ambitos;
+                    this.nodos[i].ambitos=ambi;
+                    var result_temp=this.nodos[i].execute(local);
+                    //aca no vamos a recibir ninguna etiqueta ya que solo se ejecuta el if
+                    if(result_temp!=null){  
+                        temp+=result_temp.cadena;
+                    }
+                }else if(this.nodos[i] instanceof Asignacion){
+                    var ambi=this.ambitos;
+                    this.nodos[i].ambitos=ambi;
+                    var result_temp=this.nodos[i].execute(local);
+                    if(result_temp!=null){
+                        temp+=result_temp.cadena;
+                        console.log(result_temp.cadena);
+                        temp+="//empieza la asignacion variable local\n";
+                        var temph=generarEtiqueta();
+                        temp+=temph+"=h;\n";
+                        temp+="heap[h]="+result_temp.u_etiqueta+";\n";
+                        temp+="h=h+1;\n";
+                        var simulado=generarEtiqueta();
+                        var sim=local.obtener(this.nodos[i].id+"_"+ambi);
+                        temp+=simulado+"=p+"+sim.posRel+";\n";
+                        temp+="stack["+simulado+"]="+temph+";\n";
+                        temp+="//fin asignacion variable local\n";
+                    }else{
+                       
+                    }
+                }else if(this.nodos[i] instanceof Detener){
+                    if(pool_salida.length>0){
+                        temp+="//INICIA DETENER-------------------------\n";
+                        var temp_salida=pool_salida.pop();
+                        temp+="goto "+temp_salida+";\n";
+                        temp+="//FINALIZA DETENER-------------------------\n";
+                    }else{
+                        alert("Error Semantico, la sentencia breake no corresponde a esta seccion de codigo");
+                    }
+                }
+            }
+            temp+=result_condi.cadena;
+            temp+="if("+result_condi.u_etiqueta+"==0) goto "+etisalida+";\n";
+            temp+="goto "+etiinicio+";\n";
         }
         temp+=etisalida+":\n";
         result.cadena+=temp;
